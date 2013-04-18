0680074f14c10a857aa54a66ec029aff5fea5205
Raymond T. Hightower <rth@wisdomgroup.com>
2013-04-17 23:30:20 -0500

    started first sample app, called exercise

---
+++ exercise/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Exercise::Application.config.secret_token = '13869c067d32a10ea694bb717eaaf44b259e82431c1a35ad0eaf95078e4a8fff4c9d3f851270157e506426ff4d028e1f23871088ec7d9bbd37fb8677db4e3897'
