09b4675e837c5a93bc6941a21bb577d1825a88be
Raymond T. Hightower <rth@wisdomgroup.com>
2013-04-19 19:22:21 -0500

    added demo_app to play with scaffolding

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '3d66f8353e2c33d11c8f8990f080a9e44e4006f3698f56244904b76a146691beda45943e06571514dbafe73bcf75a04476aef34a36677d1bf1499a68a911055a'
